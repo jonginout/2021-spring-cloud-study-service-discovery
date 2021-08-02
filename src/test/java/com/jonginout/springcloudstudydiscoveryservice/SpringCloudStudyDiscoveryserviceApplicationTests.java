78cc717a96f5d5df6973b1c8e68a39c72c285b8e
jongin <jongin@toss.im>
2021-08-02 22:23:24 +0900

    init

---
+++ src/test/java/com/jonginout/springcloudstudydiscoveryservice/SpringCloudStudyDiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonginout.springcloudstudydiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStudyDiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
